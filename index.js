c08710fc646af12599dddcd6cc669c29c794290f
ckb <ckingbailey@gmail.com>
2019-10-27 10:03:13 -0700

    first successful build process 2019-10-27

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-let express = require('express')
-let { token } = require('./src/http/token')
-require('dotenv').config()
+import App from './App.js'
 
-const PORT = process.env.PORT || 3000
-
-let app = express()
-
-app.use('*', express.json())
-app.use('*', express.urlencoded({ extended: true }))
-app.use((req, res, next) => {
-    res.set('Access-Control-Allow-Origin', 'http://localhost:5000')
-    next()
-})
-
-app.all('/token', (req, res) => {
-    console.log('request received', req.body)
-    token(req, res)
-})
-
-app.listen(PORT, () => console.log('listening on port ' + PORT))
+(async function() {
+    let root = document.getElementById('main')
+    
+    let app = new App(root)
+    await app.initialize()
+    app.render()
+})()
